07b10146713c8b7bfd40d9025beab30ee9f62d51
Yxtomix <yxtomix@gmail.com>
2024-11-01 23:05:43 +0100

    update

--- backend-jmkcloud/src/Controller/DefaultController.php
+++ backend-jmkcloud/src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
@@ -30,5 +31,11 @@ class DefaultController extends AbstractController
         return $this->json([
             'message' => 'Mon test /apiv1/test'
         ]);
+    }
+
+    #[Route('/phpinfo', name: 'phpinfo')]
+    public function phpinfo(): Response
+    {
+        return new Response('<pre>' . print_r(get_loaded_extensions(), true) . '</pre>');
     }
 }
