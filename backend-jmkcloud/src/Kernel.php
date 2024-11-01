032fe6d41f2b880c61c9fadc023090c2f32c43c2
Yxtomix <yxtomix@gmail.com>
2024-11-01 19:00:42 +0100

    update

--- backend-jmkcloud/src/Kernel.php
+++ backend-jmkcloud/src/Kernel.php
@@ -8,4 +8,15 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    // Override the getCacheDir method
+    public function getCacheDir(): string
+    {
+        return '/tmp/symfony/cache/' . $this->environment;
+    }
+
+    public function getLogDir(): string
+    {
+        return '/tmp/symfony/logs';
+    }
 }
